00d8752a6abaaf217e2fa86c728378dda33039d7
chaysewlkr21 <107583005+chaysewlkr21@users.noreply.github.com>
2022-06-21 11:45:38 -0400

    Link change

--- pages/index.js
+++ pages/index.js
@@ -21,9 +21,9 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="/Questionone" className={styles.card}>
+            <h2>Question 1 &rarr;</h2>
+            <p>Answer to Question 1</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
